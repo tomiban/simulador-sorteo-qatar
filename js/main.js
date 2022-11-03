f88abe7eb6af7496be6443c84af15ca4cdbad3c3
tomiban <81537786+tomiban@users.noreply.github.com>
2022-11-03 10:51:51 -0300

    se implementa la funcion fetchData

---
+++ js/main.js
@@ -0,0 +1,144 @@
+let grupos = [
+	{
+		id: 1,
+		equipos: [],
+	},
+	{
+		id: 2,
+		equipos: [],
+	},
+	{
+		id: 3,
+		equipos: [],
+	},
+	{
+		id: 4,
+		equipos: [],
+	},
+	{
+		id: 5,
+		equipos: [],
+	},
+	{
+		id: 6,
+		equipos: [],
+	},
+	{
+		id: 7,
+		equipos: [],
+	},
+	{
+		id: 8,
+		equipos: [],
+	},
+];
+
+const btnSortear = document.querySelector('.btn-primary');
+const divGrupos = document.querySelector('#divGrupos');
+const article = document.querySelector('#article');
+const templateGrupos = document.querySelector('#templateGrupos').content;
+const fragment = document.createDocumentFragment();
+
+const fetchData = async () => {
+	try {
+		const res = await fetch('../data/selecciones.json');
+		const data = await res.json();
+		realizarSorteo(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+function nroAleatorio(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sacarBolilla(bombo) {
+	switch (bombo) {
+		case 0:
+			return nroAleatorio(1, 8);
+		case 1:
+			return nroAleatorio(9, 16);
+
+		case 2:
+			return nroAleatorio(17, 24);
+
+		case 3:
+			return nroAleatorio(25, 32);
+	}
+}
+
+const realizarSorteo = json => {
+	const repetidos = [];
+	grupos.forEach(grupo => {
+		for (let i = 0; i < 4; i++) {
+			let flag = 0;
+			while (flag === 0) {
+				const idEquipoSorteado = sacarBolilla(i);
+
+				if (!repetidos.includes(idEquipoSorteado)) {
+					repetidos.push(idEquipoSorteado);
+					//pushear seleccion.id a grupos.equipos
+					const equipo = json.selecciones.find(
+						equipo => equipo.id === idEquipoSorteado
+					);
+
+					grupo.equipos.push(equipo);
+					flag = 1;
+				}
+			}
+		}
+	});
+	localStorage.setItem('grupos', JSON.stringify(grupos));
+};
+
+const pintarGrupos = timeOut => {
+	const spinner = document.querySelector('#spinner');
+	spinner.classList.remove('d-none');
+
+	setTimeout(() => {
+		spinner.classList.add('d-none');
+
+		grupos.map(grupo => {
+			templateGrupos.querySelector('h5').textContent = `GRUPO ${grupo.id}`;
+			templateGrupos.querySelector('.list-group').innerHTML = '';
+			grupo.equipos.forEach(equipo => {
+				templateGrupos.querySelector('.list-group').innerHTML += `
+				<li
+				class="list-group-item d-flex align-items-center justify-content-evenly">
+				<div class='mx-1'<></div>
+				<div class="mx-0 bandera"><img class="text-center" src="${equipo.bandera}"></div>
+				<div class="text-center" style="width:155px;">
+				<span class="lead">${equipo.nombre}</span>
+				</div>
+				</li>
+				`;
+			});
+
+			const clone = templateGrupos.cloneNode(true);
+
+			fragment.appendChild(clone);
+		});
+		divGrupos.appendChild(fragment);
+
+		grupos.forEach(grupo => (grupo.equipos = []));
+	}, timeOut);
+
+	btnSortear.textContent = 'Volver a sortear';
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	if (localStorage.getItem('grupos')) {
+		grupos = JSON.parse(localStorage.getItem('grupos'));
+		pintarGrupos(0);
+		spinner.classList.add('d-none');
+	}
+
+	btnSortear.addEventListener('click', () => {
+		fetchData();
+		divGrupos.innerHTML = '';
+		pintarGrupos(1000);
+	});
+});
