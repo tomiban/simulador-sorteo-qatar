1dea33054dee8e7573136bf4b6d251dd87250ff1
tomiban <81537786+tomiban@users.noreply.github.com>
2022-11-08 18:42:33 -0300

    se arregla la funcion realizar sorteo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+const bombos = [
+	{
+		id: 1,
+		selecciones: [],
+	},
+	{
+		id: 2,
+		selecciones: [],
+	},
+	{
+		id: 3,
+		selecciones: [],
+	},
+	{
+		id: 4,
+		selecciones: [],
+	},
+];
+
 let grupos = [
 	{
 		id: 1,
@@ -34,16 +53,17 @@ let grupos = [
 ];
 
 const btnSortear = document.querySelector('.btn-primary');
+
 const divGrupos = document.querySelector('#divGrupos');
 const article = document.querySelector('#article');
+const templateBombos = document.querySelector('#templateBombos').content;
 const templateGrupos = document.querySelector('#templateGrupos').content;
-const fragment = document.createDocumentFragment();
 
 const fetchData = async () => {
 	try {
 		const res = await fetch('./data/selecciones.json');
 		const data = await res.json();
-		realizarSorteo(data);
+		initApp(data);
 	} catch (error) {
 		console.log(error);
 	}
@@ -55,110 +75,133 @@ function nroAleatorio(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function sacarBolilla(bombo) {
-	switch (bombo) {
-		case 0:
-			return nroAleatorio(2, 8);
-		case 1:
-			return nroAleatorio(9, 16);
-
-		case 2:
-			return nroAleatorio(17, 24);
-
-		case 3:
-			return nroAleatorio(25, 32);
-	}
+function sacarBolilla() {
+	return nroAleatorio(0, 7);
 }
 
 function reiniciarSorteo() {
 	grupos.forEach(grupo => {
-		grupo.selecciones = [];
+		grupo.selecciones.length = 0;
 	});
+	realizarSorteo(bombos)
 }
 
-const realizarSorteo = json => {
-	const repetidos = [];
+function limiteConfederacion(idGrupoSorteado, seleccion) {
+	const contadores = {
+		UEFA: 0,
+		CONMEBOL: 0,
+		CONCACAF: 0,
+		AFC: 0,
+		CAF: 0,
+	};
 
-	grupos[0].selecciones.push(json.selecciones[0]);
+	grupos[idGrupoSorteado].selecciones.forEach(equipo => {
+		contadores[equipo.confederacion]++;
+	});
 
-	grupos.forEach(grupo => {
-		const contadores = {
-			UEFA: 0,
-			CONMEBOL: 0,
-			CONCACAF: 0,
-			AFC: grupo.id === 1 ? 1 : 0,
-			CAF: 0,
-			ATTEMPS: 0,
-		};
+	const { UEFA, CONMEBOL, CONCACAF, AFC, CAF } = contadores;
 
-		for (let i = grupo.id === 1 ? 1 : 0; i < 4; i++) {
+	switch (seleccion.confederacion) {
+		case 'UEFA':
+			return UEFA === 2;
+		case 'CONMEBOL':
+			return CONMEBOL === 1;
+		case 'CONCACAF':
+			return CONCACAF === 1;
+		case 'AFC':
+			return AFC === 1;
+		case 'CAF':
+			return CAF === 1;
+	}
+}
+
+const realizarSorteo = bombos => {
+	//Entra a cada bombo
+	bombos.forEach(bombo => {
+		const repetidos = [];
+		//Entra a los equipos del bombo y sortea el grupo que va integrar cada uno, no pueden ir al mismo
+		bombo.selecciones.forEach(seleccion => {
+			let ATTEMPS = 0;
 			let flag = 0;
 			while (flag === 0) {
-				const idEquipoSorteado = sacarBolilla(i);
-				if (!repetidos.includes(idEquipoSorteado)) {
-					const equipo = json.selecciones.find(
-						equipo => equipo.id === idEquipoSorteado
-					);
-
-					const { UEFA, CONMEBOL, CONCACAF, AFC, CAF, ATTEMPS } = contadores;
-
-					if (ATTEMPS === 100) {
-						reiniciarSorteo();
-						realizarSorteo(json);
-						return;
+				ATTEMPS++
+				let idGrupoSorteado = sacarBolilla();
+				//Omito al anfitrion del sorteo
+				if (seleccion.nombre === 'Qatar') {
+					grupos[0].selecciones.push(seleccion);
+					idGrupoSorteado = 0;
+					repetidos.push(0);
+					flag = 1;
+				} else {
+					//Compruebo si ya no salió sorteado ese grupo
+					if (!repetidos.includes(idGrupoSorteado)) {
+						//Compruebo si respeto el limite de conferencias x grupo
+						if (!limiteConfederacion(idGrupoSorteado, seleccion)) {
+							grupos[idGrupoSorteado].selecciones.push(seleccion);
+							repetidos.push(idGrupoSorteado);
+							flag = 1;
+						}
 					}
-
-					switch (equipo.confederacion) {
-						case 'UEFA':
-							if (UEFA < 2) {
-								repetidos.push(idEquipoSorteado);
-								contadores[equipo.confederacion]++;
-								grupo.selecciones.push(equipo);
-								flag = 1;
-							}
-							break;
-						case 'CONMEBOL':
-							if (CONMEBOL < 1) {
-								repetidos.push(idEquipoSorteado);
-								contadores[equipo.confederacion]++;
-								grupo.selecciones.push(equipo);
-								flag = 1;
-							}
-							break;
-						case 'CONCACAF':
-							if (CONCACAF < 1) {
-								repetidos.push(idEquipoSorteado);
-								contadores[equipo.confederacion]++;
-								grupo.selecciones.push(equipo);
-								flag = 1;
-							}
-							break;
-						case 'AFC':
-							if (AFC < 1) {
-								repetidos.push(idEquipoSorteado);
-								contadores[equipo.confederacion]++;
-								grupo.selecciones.push(equipo);
-								flag = 1;
-							}
-							break;
-						case 'CAF':
-							if (CAF < 1) {
-								repetidos.push(idEquipoSorteado);
-								contadores[equipo.confederacion]++;
-								grupo.selecciones.push(equipo);
-								flag = 1;
-							}
-							break;
-					}
-					contadores['ATTEMPS']++;
+				}
+				//si el sorteo queda imposibilitado por el max de conferencias se hace de nuevo (ERROR --< pushea los equipos restantes del sorteo fallado)
+				if (ATTEMPS === 100) {
+					reiniciarSorteo()
+					return
 				}
 			}
-		}
+		});
 	});
+
 	localStorage.setItem('grupos', JSON.stringify(grupos));
 };
 
+
+const pintarBombos = data => {
+	const selecciones = data.selecciones;
+	selecciones.forEach(seleccion => {
+		if (seleccion.id <= 8) {
+			bombos[0].selecciones.push(seleccion);
+		}
+		if (seleccion.id > 8 && seleccion.id <= 16) {
+			bombos[1].selecciones.push(seleccion);
+		}
+		if (seleccion.id > 16 && seleccion.id <= 24) {
+			bombos[2].selecciones.push(seleccion);
+		}
+		if (seleccion.id > 24 && seleccion.id <= 32) {
+			bombos[3].selecciones.push(seleccion);
+		}
+	});
+
+	const fragment = document.createDocumentFragment();
+
+	bombos.forEach(bombo => {
+		templateBombos.querySelector('h5').textContent = `BOMBO ${bombo.id}`;
+		templateBombos.querySelector('.list-group').innerHTML = '';
+		bombo.selecciones.forEach(seleccion => {
+			templateBombos.querySelector('.list-group').innerHTML += `
+			<li class="list-group-item d-flex align-items-center justify-content-evenly">
+			<div class='mx-1'></div>
+			<div class="bandera">
+					<img class="text-center" src="${seleccion.bandera}" /> </div>	
+				<div class="" style="width: 155px">
+					<span class="lead mx-1">${seleccion.nombre}</span>
+				</div>
+			</li>
+			`;
+		});
+
+		const clone = templateBombos.cloneNode(true);
+
+		fragment.appendChild(clone);
+	});
+
+	divGrupos.appendChild(fragment);
+};
+
 const pintarGrupos = timeOut => {
+	
+	const fragment = document.createDocumentFragment();
 	const spinner = document.querySelector('#spinner');
 
 	spinner.classList.remove('d-none');
@@ -186,6 +229,7 @@ const pintarGrupos = timeOut => {
 
 			fragment.appendChild(clone);
 		});
+		divGrupos.innerHTML = ''
 		divGrupos.appendChild(fragment);
 
 		grupos.forEach(grupo => (grupo.selecciones = []));
@@ -195,42 +239,45 @@ const pintarGrupos = timeOut => {
 	}, timeOut);
 };
 
-const initApp = () => {
-	divGrupos.innerHTML = '';
-	fetchData();
-	pintarGrupos(1000);
+const initApp = data => {
+	pintarBombos(data);
 };
+
+btnSortear.addEventListener('click', () => {
+	btnSortear.className += ' d-none';
+	if (btnSortear.textContent === 'Sortear') {
+		realizarSorteo(bombos);
+		pintarGrupos(1000);
+		return;
+	}
+
+	Swal.fire({
+		title: '¿Quieres reiniciar el sorteo?',
+		text: '¡Se modificaran los resultados!',
+		iconHtml:
+			'<img src="./img/world-cup2.png" class="rounded-circle border border-3  border-dark">',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		cancelButtonText: 'Cancelar',
+		confirmButtonText: 'Aceptar',
+	}).then(result => {
+		if (result.isConfirmed) {
+			realizarSorteo(bombos)
+			divGrupos.innerHTML = '';
+			pintarGrupos(1000)
+		} else {
+			btnSortear.classList.remove('d-none');
+		}
+	});
+});
 
 document.addEventListener('DOMContentLoaded', () => {
 	if (localStorage.getItem('grupos')) {
 		grupos = JSON.parse(localStorage.getItem('grupos'));
 		pintarGrupos(0);
 		spinner.classList.add('d-none');
+	} else {
+		fetchData();
 	}
-
-	btnSortear.addEventListener('click', () => {
-		btnSortear.className += ' d-none';
-		if (btnSortear.textContent === 'Sortear') {
-			initApp();
-			return;
-		}
-
-		Swal.fire({
-			title: '¿Quieres reiniciar el sorteo?',
-			text: '¡Se modificaran los resultados!',
-			iconHtml:
-				'<img src="./img/world-cup2.png" class="rounded-circle border border-3  border-dark">',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			cancelButtonText: 'Cancelar',
-			confirmButtonText: 'Aceptar',
-		}).then(result => {
-			if (result.isConfirmed) {
-				initApp();
-			} else {
-				btnSortear.classList.remove('d-none');
-			}
-		});
-	});
 });
